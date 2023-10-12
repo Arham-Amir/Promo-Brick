544a556f48da4815dbdabdb5a399b9412f9a4c17
Arham Amir <arhamamir424@gmail.com>
2023-10-12 12:43:03 +0500

    adding links to project

--- src/components/base/nav_links.jsx
+++ src/components/base/nav_links.jsx
@@ -1,18 +1,19 @@
 import { Link as Lin } from "react-scroll";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const Nav_Links = (props = {}) => {
   const path = usePathname();
   return (<>
     <section className={`${props.className}`}>
       <section className={`${props.childClass1} flex`}>
-        <Lin className={`${props.childClass2} hover:bg-red-100 transition-all duration-100 ease-in-out cursor-pointer`} to="home" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>HOME </Lin>
-        <Lin className={`${props.childClass2} hover:bg-red-100 transition-all duration-100 ease-in-out cursor-pointer`} to="contactus" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>BUSINESS</Lin>
-        <Lin className={`${props.childClass2} hover:bg-red-100 transition-all duration-100 ease-in-out cursor-pointer`} to="contactus" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>GALLERY</Lin>
+        <Link href='/' className={`${props.childClass2} hover:bg-themeColor transition-all duration-100 ease-in-out cursor-pointer`} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>HOME</Link>
+        <Lin className={`${props.childClass2} hover:bg-themeColor transition-all duration-100 ease-in-out cursor-pointer`} to="contactus" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>BUSINESS</Lin>
+        <Lin className={`${props.childClass2} hover:bg-themeColor transition-all duration-100 ease-in-out cursor-pointer`} to="contactus" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>GALLERY</Lin>
       </section>
       <section className={`${props.childClass1} flex`}>
-        <Lin className={`${props.childClass2} hover:bg-red-100 transition-all duration-100 ease-in-out cursor-pointer`} to="about" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>ABOUT</Lin>
-        <Lin className={`${props.childClass2} hover:bg-red-100 transition-all duration-100 ease-in-out cursor-pointer`} to="contactus" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>CONTACT-US</Lin>
+        <Lin className={`${props.childClass2} hover:bg-themeColor transition-all duration-100 ease-in-out cursor-pointer`} to="about" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>ABOUT</Lin>
+        <Lin className={`${props.childClass2} hover:bg-themeColor transition-all duration-100 ease-in-out cursor-pointer`} to="contactus" spy={true} smooth={true} duration={500} onClick={() => { document.body.classList.remove('overflow-hidden'); props.showHam?.() }}>CONTACT-US</Lin>
       </section>
     </section>
   </>
--- src/components/base/navbar.jsx
+++ src/components/base/navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
     <section ref={navRef} className={`bg-black ${ham ? 'h-[14vh]' : 'h-[14vh]'}  flex flex-col z-50 w-screen backdrop-blur-[1px]`}>
       <nav className="bg-white h-full w-full 2xl:h-28 text-black py-3 lg:pl-8 lg:pr-12 xl:pl-10 xl:pr-24 sm:px-10 xs:px-4 lg:text-md 2xl:text-xl 3xl:text-2xl flex items-center justify-between font-sans relative">
         <Nav_Logo></Nav_Logo>
-        <Nav_Links childClass2="px-3 py-1" className='xs:hidden md:flex md:justify-between w-full md:gap-7 lg:gap-12 font-semibold' />
+        <Nav_Links childClass1 = "gap-3" childClass2="px-3 py-1" className='xs:hidden md:flex md:justify-between w-full md:gap-7 lg:gap-12 font-semibold' />
         <section onClick={() => { showHam(!ham) }} className="z-30 md:hidden xs:flex flex-col items-end justify-center gap-2 pr-4" >
           <p className={`${ham ? 'w-6 rotate-[495deg] translate-y-[10px]' : 'w-8'}  transition-all duration-500 p-[1px] bg-black`}></p>
           <p className={`w-6 p-[1px] transition-all duration-500 bg-black ${ham ? 'opacity-0' : 'opacity-100'} transform ${ham ? '-translate-y-10' : 'translate-y-0'}`}></p>
