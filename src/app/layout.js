bd75aa02e3196e340e0cf9c62c714a1734eda3d4
Arham Amir <arhamamir424@gmail.com>
2023-11-18 21:46:40 +0500

    show event on page load

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import ContactUs from '@components/base/contactUs/contactUs'
 import 'react-toastify/dist/ReactToastify.css';
 import ContactPopUpBtn from '@components/base/contactPopUpBtn'
 import UpComingEvent from '@components/base/upComingEvent'
+import ShowEvent from '@components/base/showEvent'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${glass.variable} ${figtree.variable} ${inter.className} overflow-x-hidden max-w-screen font-text bg-bg scrollbar`}>
+        <ShowEvent />
         <ContactPopUpBtn />
         <UpComingEvent />
         <Navbar />
